ece5f76422deed207e271e35e5f427cdc9b11817
Gonzalo_CR <gonzalocorralesruiz@gmail.com>
2024-08-15 22:21:57 -0300

    actualizacion de database.js con variables de entorno para mayor seguridad

--- src/database.js
+++ src/database.js
@@ -1,10 +1,13 @@
-import  mysqlConnection from 'mysql2/promise';
+import mysqlConnection from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const properties = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'rest-api'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 };
 
 export const pool = mysqlConnection.createPool(properties);
